50b30fbfe8c0f165f5f1cf8aa9131d12c299bfe8
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-11-19 19:34:30 +0500

    Update WelcomeScreen.h

--- Umaima/WelcomeScreen.h
+++ Umaima/WelcomeScreen.h
@@ -1,19 +1,20 @@
 #pragma once
 #include <raylib.h>
 #include <vector>
+#include <string>
 
 using namespace std;
 
 struct FallingBlock {
     Vector2 position;
     Vector2 targetPosition;
-    Vector2 originalPosition; // Add this to store the exact target position
+    Vector2 originalPosition;
     float speed;
     Color color;
     bool arrived;
     float delay;
-    float wiggleTime;        // Add for wiggle animation
-    float wiggleOffset;      // Add for random wiggle timing
+    float wiggleTime;
+    float wiggleOffset;
 };
 
 class WelcomeScreen {
@@ -23,21 +24,26 @@ private:
     vector<FallingBlock> blocks;
     float timeElapsed;
     bool animationComplete;
-    bool wigglePhase;        // Add to track if we're in wiggle phase
+    bool wigglePhase;
 
-    // Button
+    // Buttons
     int playButtonX, playButtonY;
+    int instructionsButtonX, instructionsButtonY;
     bool isHoveringPlay;
+    bool isHoveringInstructions;
     Color buttonColor;
+    bool showInstructions;  // NEW: Track if instructions are shown
 
 public:
     WelcomeScreen();
     ~WelcomeScreen();
 
     void Load();
-    void Update(bool& startGame);
+    void Update(bool& startGame, bool& showInstructionsScreen);
     void Draw();
     void Unload();
+    bool GetShowInstructions() const { return showInstructions; }  // NEW: Getter
+    void DrawInstructions();  // NEW: Draw instructions screen
 
 private:
     void CreateLetterT(float startX, float startY);
