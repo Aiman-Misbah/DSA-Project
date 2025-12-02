4fd74556ffcde2ddffdc10efb0a547fb0e1df687
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-12-03 00:45:44 +0500

    Add UI colors for PanelBlue and DarkOverlay
    Added new UI colors for Panel and Dark Overlay.

--- Umaima/Colours.cpp
+++ Umaima/Colours.cpp
@@ -3,6 +3,7 @@
 #include <raylib.h>
 #include <vector>
 using namespace std;
+
 //extern keyword isliye bcz then we can use this constant easily in other files as well
 const Color DarkGray = { 26, 31, 40, 255 };
 const Color Green = { 47, 230,23,255 };
@@ -14,6 +15,10 @@ const Color Cyan = { 21, 204, 209, 255 };
 const Color Blue = { 13, 64, 216, 255 };
 const Color LightBlue = { 59, 85, 162, 255 };
 const Color DarkBlue = { 44, 44, 127, 255 };
+
+// UI Colors - ADD THESE TWO LINES
+const Color PanelBlue = { 59, 85, 162, 255 };
+const Color DarkOverlay = { 0, 0, 0, 100 };
 
 vector<Color> GetCellColours() {
 	return vector <Color>{ DarkGray, Green, Red, Orange, Yellow, Purple, Cyan, Blue };
