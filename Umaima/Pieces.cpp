6772b35bda32e7162af6508934514cd4e222b3e6
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-12-03 00:48:52 +0500

    Update Pieces.cpp

--- Umaima/Pieces.cpp
+++ Umaima/Pieces.cpp
@@ -3,29 +3,29 @@
 #include "Piece.h"
 using namespace std;
 
-class LPiece:public Piece {
-	public:
-		LPiece() {
-			id = 1;
-			cells[0] = { Position(0,2), Position(1,0), Position(1,1), Position(1,2) };
-			cells[1] = { Position(0,1), Position(1,1), Position(2,1), Position(2,2) };
-			cells[2] = { Position(1,0), Position(1,1),Position(1,2),Position(2,0) };
-			cells[3] = { Position(0,0),Position(0,1),Position(1,1), Position(2,1) };
-			Move(0, 3);
-		}
+class LPiece :public Piece {
+public:
+	LPiece() {
+		id = 1;
+		cells[0] = { Position(0,2), Position(1,0), Position(1,1), Position(1,2) };
+		cells[1] = { Position(0,1), Position(1,1), Position(2,1), Position(2,2) };
+		cells[2] = { Position(1,0), Position(1,1),Position(1,2),Position(2,0) };
+		cells[3] = { Position(0,0),Position(0,1),Position(1,1), Position(2,1) };
+		Move(0, 3);
+	}
 };
 
 
 class JPiece :public Piece {
-	public:
-		JPiece() {
-			id = 2;
-			cells[0] = { Position(0,0), Position(1,0), Position(1,1), Position(1,2) };
-			cells[1] = { Position(0,1), Position(0,2), Position(1,1), Position(2,1) };
-			cells[2] = { Position(1,0), Position(1,1), Position(1,2), Position(2,2) };
-			cells[3] = { Position(0,1), Position(1,1), Position(2,0), Position(2,1) };
-			Move(0, 3);
-		}
+public:
+	JPiece() {
+		id = 2;
+		cells[0] = { Position(0,0), Position(1,0), Position(1,1), Position(1,2) };
+		cells[1] = { Position(0,1), Position(0,2), Position(1,1), Position(2,1) };
+		cells[2] = { Position(1,0), Position(1,1), Position(1,2), Position(2,2) };
+		cells[3] = { Position(0,1), Position(1,1), Position(2,0), Position(2,1) };
+		Move(0, 3);
+	}
 };
 
 class IPiece :public Piece {
@@ -71,7 +71,7 @@ public:
 		cells[0] = { Position(0,1), Position(1,0), Position(1,1), Position(1,2) };
 		cells[1] = { Position(0,1), Position(1,1), Position(1,2), Position(2,1) };
 		cells[2] = { Position(1,0), Position(1,1), Position(1,2), Position(2,1) };
-		
+
 		cells[3] = { Position(0,1), Position(1,0), Position(1,1), Position(2,1) };
 		Move(0, 3);
 	}
