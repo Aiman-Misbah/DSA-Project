d682057bf3c39f071d7a4d6e8d2d1ed3fa4e5134
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-12-03 00:47:37 +0500

    Add game over screen handling and particle system

--- Umaima/Manager.h
+++ Umaima/Manager.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <raylib.h>
 #include <vector>
+#include "Leaderboard.h"
 #include <iostream>
 using namespace std;
 
@@ -22,13 +23,41 @@ private:
     float musicAnimationProgress;
     bool isMusicAnimating;
 
+    void DrawGameOverScreen(Font& font, int score, bool& gameOver, bool& restartRequested, bool& returnToMenuRequested);
+    void UpdateGameOverScreen(Vector2 mousePos, bool& gameOver, bool& restartRequested, bool& returnToMenuRequested);
+
+    // Game Over screen variables
+    Rectangle replayButton;
+    Rectangle menuButton;
+    bool isHoveringReplay;
+    bool isHoveringMenu;
+
+    // Particle system
+    struct GameOverParticle {
+        Vector2 position;
+        Vector2 velocity;
+        Color color;
+        float size;
+        float life;
+    };
+    std::vector<GameOverParticle> particles;
+    void UpdateParticles(float deltaTime);
+    void DrawParticles();
+    void CreateParticles();
+
+    float leaderboardX, leaderboardY;
+    float scoreDisplayX, scoreDisplayY;
 
 public:
     Manager();
     ~Manager();
 
     void LoadTextures();
-    void Update(bool& musicOn, bool& gamePaused, bool isCountingDown, bool& ghostEnabled, float& ghostAnimationProgress);
-    void Draw(bool musicOn, bool gamePaused, int score, Font& font, bool ghostEnabled, float ghostAnimationProgress, double time, int lines);
+    void Update(bool& musicOn, bool& gamePaused, bool isCountingDown, bool& ghostEnabled,
+        float& ghostAnimationProgress, bool gameOver, bool& restartRequested, bool& returnToMenuRequested);
+
+    // Draw method signature needs to change
+    void Draw(bool musicOn, bool gamePaused, int score, Font& font, bool ghostEnabled,
+        float ghostAnimationProgress, double time, int lines, bool gameOver);
     void UnloadTextures();
 };
