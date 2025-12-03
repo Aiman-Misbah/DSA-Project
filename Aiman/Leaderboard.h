2b1ade19b44a7d7591483ac34a83bca64358d964
Umaima Manzoor <121282635+Umaima-Manzoor@users.noreply.github.com>
2025-12-03 11:55:02 +0500

    Update Leaderboard.h

--- Aiman/Leaderboard.h
+++ Aiman/Leaderboard.h
@@ -1,26 +1,17 @@
-// Leaderboard.h
 #pragma once
 #include "ScoreAVL.h"
+#include <iostream>
 #include <vector>
-#include <string>
-#include <fstream>
 using namespace std;
 
 class Leaderboard {
 private:
     ScoreAVL scoresTree;
-    string filename = "leaderboard.txt";
-
-    void SaveScoresToFile();
-    void LoadScoresFromFile();
 
 public:
-    Leaderboard();
-    ~Leaderboard();
 
     void AddScore(int score);
     vector<int> GetTopScores(int count);
     int GetHighestScore();
     void Reset();
-    void Display();  // For debugging
 };
